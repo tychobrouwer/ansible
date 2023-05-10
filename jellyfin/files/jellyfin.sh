dd45a287c1321311f166f37c1cc00f60ad8386a8
Tycho Brouwer <tychobtouwer33@gmail.com>
2023-05-10 23:12:15 +0200

    restructering

---
+++ jellyfin/files/jellyfin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+JELLYFINDIR="/opt/jellyfin"
+FFMPEGDIR="/usr/share/jellyfin-ffmpeg"
+
+$JELLYFINDIR/jellyfin \
+ -d $JELLYFINDIR/data \
+ -C $JELLYFINDIR/cache \
+ -c $JELLYFINDIR/config \
+ -l $JELLYFINDIR/log \
+ --ffmpeg $FFMPEGDIR/ffmpeg
+
--- proxmox/pbs-proxmox-backup.template.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-export PBS_PASSWORD="---REDACTED---"
-export PBS_FINGERPRINT="---REDACTED---"
-export PBS_LOG="warn"
-export PXAR_LOG="warn"
-
-/usr/bin/proxmox-backup-client backup root.pxar:/media/file-share --repository root@pam@192.168.178.108:main
